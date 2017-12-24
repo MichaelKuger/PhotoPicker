eaa7959896d7417a1a5b5a5e024a00a64577709d
mkuger <michael.kuger@gmail.com>
2017-12-25 00:45:22 +0100

    tests created, test resources added

---
+++ src/test/java/photopicker/imaging/ImageTest.java
@@ -0,0 +1,48 @@
+package photopicker.imaging;
+
+import org.junit.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ImageTest {
+
+    @Test
+    public void testLandscapeMode() throws URISyntaxException, ImagingException, IOException {
+        File imageFile = getFile("SAM_3977.JPG");
+        BufferedImage image = ImageUtils.loadImage(imageFile);
+        assertEquals(1024, image.getWidth());
+        assertEquals(683, image.getHeight());
+        writeImage(image);
+    }
+
+    @Test
+    public void testPortraitMode() throws IOException, URISyntaxException, ImagingException {
+        File imageFile = getFile("SAM_3990.JPG");
+        BufferedImage image = ImageUtils.loadImage(imageFile);
+        assertEquals(512, image.getWidth());
+        assertEquals(768, image.getHeight());
+        writeImage(image);
+    }
+
+    private void writeImage(BufferedImage image) throws IOException {
+        File outputFile = File.createTempFile("photopicker-", ".jpg");
+        outputFile.deleteOnExit();
+        System.out.println(outputFile.getAbsolutePath());
+        ImageIO.write(image, "jpg", outputFile);
+        // breakpoint here. Files get deleted after VM shutdown
+        System.out.println("foo");
+    }
+
+    private File getFile(String filename) throws URISyntaxException {
+        URL url = getClass().getResource("/" + filename);
+        return new File(url.toURI());
+    }
+}
